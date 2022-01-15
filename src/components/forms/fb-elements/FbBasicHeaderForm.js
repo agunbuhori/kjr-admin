01afa09368fd07ee6868ebfd0fb6dd2f1d24a722
agunbuhori <agun.buhori@gmail.com>
2022-01-15 17:14:44 +0700

    autorefresh added

--- src/components/forms/fb-elements/FbBasicHeaderForm.js
+++ src/components/forms/fb-elements/FbBasicHeaderForm.js
@@ -29,8 +29,8 @@ const formReducer = (state, action) => {
 
 function makeid(length) {
   let result           = '';
-  let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let charactersLength = characters.length;
+  const characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
   for ( let i = 0; i < length; i++ ) {
     result += characters.charAt(Math.floor(Math.random() * 
 charactersLength));
